6aebee67b8ef7122aea4f64cb033a2c4f7bc7a63
HerrChaos <cedrik.zwick@gmx.net>
2024-02-16 19:00:31 +0100

    made item drops and fixed your fucked bug

--- src/main/java/chaos/frost/mixin/FrostWalkerRefactor.java
+++ src/main/java/chaos/frost/mixin/FrostWalkerRefactor.java
@@ -5,6 +5,7 @@ import chaos.frost.block.FrostedMagmaBlock;
 import chaos.frost.block.ModBlocks;
 import net.minecraft.block.*;
 import net.minecraft.enchantment.FrostWalkerEnchantment;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
@@ -46,20 +47,22 @@ public abstract class FrostWalkerRefactor {
 
             Block currentBlock = world.getBlockState(currentPos).getBlock();
 
-            if ((world.getBlockState(currentPos) == FrostedIceBlock.getMeltedState() || currentBlock == ModBlocks.FROSTED_MAGMA) && level >= NewFrostwalker.CONFIG.MaxLevel()) {
+            if ((world.getBlockState(currentPos) == FrostedMagmaBlock.getMeltedState() || currentBlock == ModBlocks.FROSTED_MAGMA) && level >= NewFrostwalker.CONFIG.MaxLevel()) {
                 if (!frostedMagmaState.canPlaceAt(world, currentPos) || !world.canPlace(frostedMagmaState, currentPos, ShapeContext.absent()))
                     continue;
                 world.setBlockState(currentPos, frostedMagmaState);
                 world.scheduleBlockTick(currentPos, ModBlocks.FROSTED_MAGMA, MathHelper.nextInt(user.getRandom(), 60, 120));
             }
 
-            // TODO: Maybe drop the kelp/seagrass as an item.
-            //  Maybe use a tag instead of hardcoding kelp and seagrass?
-            if (world.getBlockState(currentPos) == FrostedMagmaBlock.getMeltedState() || currentBlock == Blocks.FROSTED_ICE || (world.getBlockState(currentPos).getFluidState().isIn(FluidTags.WATER)
+            // TODO: Maybe drop the kelp/seagrass as an item. -DONE
+            //  Maybe use a tag instead of hardcoding kelp and seagrass? HOW??
+            if (world.getBlockState(currentPos) == FrostedIceBlock.getMeltedState() || currentBlock == Blocks.FROSTED_ICE || (world.getBlockState(currentPos).getFluidState().isIn(FluidTags.WATER)
                     && (currentBlock == Blocks.KELP || currentBlock == Blocks.SEAGRASS))) {
                 if (!frostedIceState.canPlaceAt(world, currentPos) || !world.canPlace(frostedIceState, currentPos, ShapeContext.absent()))
                     continue;
+                ItemEntity items = new ItemEntity(user.getWorld(), currentPos.getX(), currentPos.getY(), currentPos.getZ(), currentBlock.asItem().getDefaultStack());
                 world.setBlockState(currentPos, frostedIceState);
+                world.spawnEntity(items);
                 world.scheduleBlockTick(currentPos, Blocks.FROSTED_ICE, MathHelper.nextInt(user.getRandom(), 60, 120));
             }
         }
